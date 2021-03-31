10488f9aaa9ec80705524633ec271c277b6c6afd
NitikNarang77 <nitiknarang77@gmail.com>
2021-03-31 18:58:33 +0530

    make it better

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { UserContext } from '../../App'
 import Base from '../basic/Base'
-import Particles from 'react-particles-js';
+
 import {Link, NavLink} from 'react-router-dom';
 import Menu from '../basic/Menu';
 function Home() {
